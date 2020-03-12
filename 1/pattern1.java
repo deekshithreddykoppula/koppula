ede8cd227e013b185420677b9d13bc4d2f6c468a
Aravelly <2091391@MATRIXCOLLEGE.LCL>
2020-03-12 16:23:48 -0400

    pattern1

---
+++ 1/pattern1.java
@@ -0,0 +1,17 @@
+package patterns;
+
+public class pattern1 {
+
+	public static void main(String[] args) {
+int r=8;
+for (int a=0;a<=8;a++)
+{
+	for(int b=0;b<=a;b++)
+	{
+		System.out.print("*"+" ");
+	}
+	System.out.println(" ");
+}
+	}
+
+}
